7d5ee2090bb655f69609776cfc8f9bded1466ee3
ColeChiodo <colechiodo@gmail.com>
2026-05-24 10:36:46 -0700

    moved from sdl3 to raylib

--- README.md
+++ README.md
@@ -6,16 +6,16 @@ I personally run an Arch (btw) Linux environment.
 So that is what the following guide will use.
 
 ```bash
-sudo pacman -S --needed base-devel sdl3 sdl3_image sdl3_ttf sdl3_mixer
+sudo pacman -S raylib cmake base-devel
 ```
 
 ## Build the Game
 I use CMake to build the game.
 ```bash
 # Set CMake source and build directory
-cmake -S . -B build
+cmake -B build
 # Build the Project
 cmake --build build
 # Run The Game
-./build/game
+./build/main
 ```
--- src/main.c
+++ src/main.c
@@ -1,115 +1,22 @@
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_events.h>
-#include <SDL3/SDL_init.h>
+#include "raylib.h"
 
-#include <SDL3/SDL_render.h>
-#include <SDL3/SDL_timer.h>
-#include <SDL3/SDL_video.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 600;
 
-#define SDL_FLAGS SDL_INIT_VIDEO
-#define SDL_WINDOWFLAGS (SDL_WINDOW_FULLSCREEN | SDL_WINDOW_VULKAN)
+    InitWindow(screenWidth, screenHeight, "2d multiplayer game");
 
-#define WINDOW_TITLE "Cole's Game"
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+    SetTargetFPS(60);
 
-struct Game {
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-	SDL_Event event;
-	bool is_running;
-};
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
 
-bool game_init_sdl(struct Game *g);
-bool game_new(struct Game *g);
-void game_free(struct Game *g);
-void game_events(struct Game *g);
-void game_draw(struct Game *g);
-void game_run(struct Game *g);
+    CloseWindow();
 
-int main(void) {
-	bool exit_status = EXIT_FAILURE;
-
-	struct Game game = {0};
-	
-	if (game_new(&game)) {
-		game_run(&game);
-
-		exit_status = EXIT_SUCCESS;
-	}
-	
-	game_free(&game);
-
-	return exit_status;
-}
-
-bool game_init_sdl(struct Game *g) {
-	if (!SDL_Init(SDL_FLAGS)) {
-		fprintf(stderr, "Error Initializing SDL3: %s\n", SDL_GetError());
-		return false;
-	}
-
-	g->window = SDL_CreateWindow(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOWFLAGS);
-	if (!g->window) {
-		fprintf(stderr, "Error Creating Window: %s\n", SDL_GetError());
-		return false;
-	}
-
-	g->renderer = SDL_CreateRenderer(g->window, NULL);
-	if (!g->renderer) {
-		fprintf(stderr, "Error Creating Renderer: %s\n", SDL_GetError());
-		return false;
-	}
-	
-	return true;
-}
-
-bool game_new(struct Game *g) {
-	if (!game_init_sdl(g)) {
-		return false;
-	}
-	g->is_running = true;
-	return true;
-}
-
-void game_free(struct Game *g) {
-	if (g->renderer) {
-		SDL_DestroyRenderer(g->renderer);
-		g->renderer = NULL;
-	}
-
-	if (g->window) {
-		SDL_DestroyWindow(g->window);
-		g->window = NULL;
-	}
-
-	SDL_Quit();
-}
-
-void game_events(struct Game *g) {
-	while (SDL_PollEvent(&g->event)) {
-		switch (g->event.type) {
-			case SDL_EVENT_QUIT:
-				g->is_running = false;
-				break;
-			default:
-				break;
-		}
-	}
-}
-
-void game_draw(struct Game *g) {
-	SDL_RenderClear(g->renderer);
-	SDL_RenderPresent(g->renderer);
-}
-
-void game_run(struct Game *g) {
-	while (g->is_running) {
-		game_events(g);
-		game_draw(g);
-		SDL_Delay(1000/60);
-	}
+    return 0;
 }
